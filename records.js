73229603d988ae7bdf5a8b5158aaa5a784b0ac6b
Joacim Cronwall <cronwall3@gmail.com>
2025-02-27 16:25:42 +0100

    Create records.js

---
+++ records.js
@@ -0,0 +1,17 @@
+const runningRecords = {
+    "Men": {
+        "M40": { "5000m": 786, "10000m": 1669, "Half Marathon": 3669, "Marathon": 7455 },
+        "M45": { "5000m": 851, "10000m": 1779, "Half Marathon": 3688, "Marathon": 8056 },
+        "M50": { "5000m": 891, "10000m": 1848, "Half Marathon": 3983, "Marathon": 8369 }
+    },
+    "Women": {
+        "W40": { "5000m": 904, "10000m": 1885, "Half Marathon": 4072, "Marathon": 8392 },
+        "W45": { "5000m": 955, "10000m": 1954, "Half Marathon": 4140, "Marathon": 8494 },
+        "W50": { "5000m": 1011, "10000m": 2105, "Half Marathon": 4518, "Marathon": 9065 }
+    }
+};
+
+const averageTimes = {
+    "Men": { "5000m": 1500, "10000m": 3000, "Half Marathon": 4500, "Marathon": 12000 },
+    "Women": { "5000m": 1600, "10000m": 3200, "Half Marathon": 4700, "Marathon": 12500 }
+};
